ffc70a4c75349aee06f5acd6ed4467269d02531f
Hiroki Omae <pegion6@gmail.com>
2013-11-13 18:36:55 +0900

    license added/hash iv fixed

--- EntangledInt.cs
+++ EntangledInt.cs
@@ -1,6 +1,34 @@
 ﻿/*
- * EntangledInt:
- * written by Hiroki Omae(@pigeon6), Andrew Innes
+ * Author:
+ * ===========
+ * Hiroki Omae, Andrew Innes
+ * 
+ * Lisense:
+ * ============
+ * This is free and unencumbered software released into the public domain.
+ * 
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ * 
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * For more information, please refer to <http://unlicense.org>
  */ 
 
 using UnityEngine;
@@ -12,7 +40,7 @@ public class EntangledInt {
 	private class Manager {
 		public LinkedList<EntangledInt> list;	// list of all engangled ints
 		public EntangledInt m_ie;				// at least one item must be in list
-		public int m_hashOfRegisteredItems;	// last hash of all items
+		public int m_hashOfRegisteredItems;		// last hash of all items
 
 		private static Manager s_manager;
 
@@ -44,7 +72,7 @@ public class EntangledInt {
 		}
 
 		private int _ComputeHashOfAllItems() {
-			int result = 6559;			
+			int result = 65599;
 			// Compute SDBM hash of all HackProofInt(s), as if they were contiguous in memory.
 			foreach(EntangledInt i in list) {
 				byte[] bytes = System.BitConverter.GetBytes( i._value );
@@ -104,160 +132,3 @@ public class EntangledInt {
 	}
 }
 
-//#include "stdafx.h"
-//#include <random>
-
-//class HackProofInt
-//{
-//public:
-//		
-//		HackProofInt()
-//	{
-//		Register(this);
-//		Initialise(0);
-//	}
-//	
-//	HackProofInt(int initialValue)
-//	{
-//		Register(this);
-//		Initialise(initialValue);
-//	}
-//	
-//	~HackProofInt()
-//	{
-//		Deregister(this);
-//	}
-//	
-//	void operator = (int newValue)
-//	{
-//		Initialise(newValue);
-//	}
-//	
-//	// TODO: Add the various arithmetic operators so we use these more conveniently.
-//	
-//	int GetValue() const
-//	{
-//		if (hashOfRegisteredItems == ComputeHashOfAllItems())
-//		{
-//			return m_value ^ m_mask;
-//		}
-//		
-//		throw new std::exception("Hack detected");
-//	}
-//	
-//private:
-//		
-//		static void Register(HackProofInt* that)
-//	{
-//		registeredItems.push_back(that);
-//	}
-//	
-//	static void Deregister(HackProofInt* that)
-//	{
-//		std::vector<HackProofInt*>::iterator match = std::find(registeredItems.begin(), registeredItems.end(), that);
-//		if (match != registeredItems.end())
-//		{
-//			registeredItems.erase(match);
-//		}
-//	}
-//	
-//	void Initialise(int initialValue)
-//	{
-//		// An additional item used to entangle the hash of any item with the hash of at least one other.
-//		static HackProofInt entanglement;
-//		
-//		m_mask = rand(); // TODO: Use a random generator which fills all the bits.
-//		m_value = initialValue ^ m_mask;
-//		
-//		// This ensures the old hash is no longer valid.
-//		if (this != &entanglement)
-//		{
-//			entanglement = rand();
-//		}
-//		
-//		hashOfRegisteredItems = ComputeHashOfAllItems();
-//	}
-//	
-//	static int ComputeHashOfAllItems()
-//	{
-//		int result = 6559;
-//		
-//		// Compute SDBM hash of all HackProofInt(s), as if they were contiguous in memory.
-//		std::vector<HackProofInt*>::const_iterator iter = registeredItems.begin();
-//		while (iter != registeredItems.end())
-//		{
-//			HackProofInt* that = *iter++;
-//			const unsigned char* readPtr = reinterpret_cast<const unsigned char*>(that);
-//			int loop = sizeof(HackProofInt);
-//			while (loop--)
-//			{
-//				unsigned char c = *readPtr++;
-//				result = c + (result << 6) + (result << 16) - result;
-//			}
-//		}
-//		
-//		return result;
-//	}
-//	
-//	// The obfuscated value.
-//	int m_value;
-//	
-//	// XOR bitmask used to (de)obfuscate value.
-//	int m_mask;
-//	
-//	// The address of all fully constructed items.
-//	static std::vector<HackProofInt*> registeredItems;
-//	
-//	// Hash value of all fully constructed items.
-//	static int hashOfRegisteredItems;
-//	
-//	friend class Hacker;
-//};
-//
-//std::vector<HackProofInt*> HackProofInt::registeredItems;
-//
-//int HackProofInt::hashOfRegisteredItems;
-//
-//class Hacker
-//{
-//public:
-//		
-//		static int GetHash()
-//	{
-//		return HackProofInt::hashOfRegisteredItems;
-//	}
-//	
-//	static void SetHash(int newValue)
-//	{
-//		HackProofInt::hashOfRegisteredItems = newValue;
-//	}
-//};
-//
-//int _tmain(int argc, _TCHAR* argv[])
-//{
-//	HackProofInt goldCoins(1234);
-//	
-//	_ASSERT(goldCoins.GetValue() == 1234);
-//	
-//	goldCoins = 999;
-//	_ASSERT(goldCoins.GetValue() == 999);
-//	
-//	{
-//		// Jot down the in-memory state, and also the hash.
-//		unsigned char oldState[sizeof(goldCoins)];
-//		memcpy(&oldState, &goldCoins, sizeof(oldState));
-//		int oldHash = Hacker::GetHash();
-//		
-//		// Spend the gold coins.
-//		goldCoins = 345;
-//		
-//		// Hack the previous in-memory state back into place, and restore the hash.
-//		memcpy(&goldCoins, &oldState, sizeof(oldState));
-//		Hacker::SetHash(oldHash);
-//		
-//		// This throws an exception, because the hack is detected.
-//		int value = goldCoins.GetValue();
-//	}
-//	
-//	return 0;
-//}
